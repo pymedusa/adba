7da3c562774eb9a9cb328f8d116b4f3f4a12b242
Chris Brumbaugh <cbrumbau@gmail.com>
2014-05-26 20:11:40 -0700

    Fixed parsing positional arguments for files with argparse, added new mylistaddwithfields command, reformatted fields output to multiple lines

--- adbacli.py
+++ adbacli.py
@@ -14,15 +14,13 @@ blacklistFields = list(('unused', 'retired', 'reserved'))
 fileTypes = '.avi,.mp4,.mkv,.ogm'
 
 parser = argparse.ArgumentParser()
-parser.add_argument('command', nargs=1, choices=['hash', 'mylistadd', 'mylistdel', 'getfields', 'listfields'],
+parser.add_argument('command', choices=['hash', 'mylistadd', 'mylistdel', 'mylistaddwithfields', 'getfields', 'listfields'],
 help='Command to execute. hash: Hash the file and print its ed2k hash. mylistadd: Hash the file and add it to AniDB MyList. If the file was there, it will be updated. mylistdel: Hash the file and delete it from AniDB MyList. getfields: Hash the file and retrieve requested fields for the hashed file. listfields: Lists all the available fields that can be requested from AniDB.')
-parser.add_argument('args', nargs=argparse.REMAINDER,
-help='All files and/or folders to be processed.')
 parser.add_argument('--file-types', default=fileTypes,
 help='A comma delmited list of file types to be included when searching directories. Default: ' + fileTypes)
 parser.add_argument('--out-file', action='store', default=None,
 help='Write output to specified file instead of STDOUT.')
-parser.add_argument('-u', '--user', action='store', default=None,
+parser.add_argument('-u', '--username', action='store', default=None,
 help='User name needed to communicate with AniDB.')
 parser.add_argument('-p', '--password', action='store', default=None,
 help='Password needed to communicate with AniDB.')
@@ -38,8 +36,18 @@ parser.add_argument('--other', action='store', default=None,
 help='Sets other remarks (any string).')
 parser.add_argument('--fields', action='store', default=None,
 help='A comma delimited list of fields requested from AniDB.')
+parser.add_argument('files', nargs='*', default=[],
+help='All files and/or folders to be processed.')
 
-args = parser.parse_args()
+args, otherArgs = parser.parse_known_args()
+
+fileParser = argparse.ArgumentParser()
+fileParser.add_argument('files', nargs='*', default=[],
+help='All files and/or folders to be processed.')
+
+fileArgs = fileParser.parse_args(otherArgs)
+
+allFiles = args.files + fileArgs.files
 
 # Function to convert an object's attributes to a dictionary for lookup
 def attrToDict(obj):
@@ -50,6 +58,16 @@ def attrToDict(obj):
 			pr[name] = value
 	return pr
 
+# Redirect sys.stdout if requested
+if args.out_file:
+	sys.stdout = open(args.out_file, 'w')
+
+# Check if fields are required
+if args.command in ['mylistaddwithfields', 'getfields']:
+	if not args.fields:
+		print("Fields to retrieve are required for " + args.command + ".")
+		sys.exit(0)
+
 # Convert state to UDP
 args.state = stateToUDP[args.state]
 
@@ -59,14 +77,10 @@ if args.watched:
 else:
 	args.watched=viewedToUDP['unwatched']
 
-# Redirect sys.stdout if requested
-if args.out_file:
-	sys.stdout = open(args.out_file, 'w')
-
 # Parse positional arguments passed in and convert all to files
 fileList = list()
 validExtensions = args.file_types.lower().split(',')
-for entry in list(args.args):
+for entry in list(allFiles):
 	if os.path.isfile(entry):
 		if any(os.path.splitext(entry)[1].lower() == valid for valid in validExtensions):
 			fileList.append(entry)
@@ -76,27 +90,30 @@ for entry in list(args.args):
 				if any(os.path.splitext(filename)[1].lower() == valid for valid in validExtensions):
 					fileList.append(filename)
 
-if args.command[0] in ['hash', 'mylistadd', 'mylistdel', 'getfields']:
+# Check if files are required
+if args.command in ['hash', 'mylistadd', 'mylistdel', 'mylistaddwithfields', 'getfields']:
 	if len(fileList) == 0:
-		print("Files and/or directories containing valid files are required for " + args.command[0] + ".")
+		print("Files and/or directories containing valid files are required for " + args.command + ".")
 		sys.exit(0)
 
-if args.command[0] in ['mylistadd', 'mylistdel', 'getfields']:
-	if not args.user or not args.password:
-		print("User and password required for " + args.command[0] + ".")
+# Check if login is required and create connection if have login credentials
+if args.command in ['mylistadd', 'mylistdel', 'mylistaddwithfields', 'getfields']:
+	if not args.username or not args.password:
+		print("User and password required for " + args.command + ".")
 		sys.exit(0)
 	connection = adba.Connection(log=False)
 	try:
-		connection.auth(args.user, args.password)
+		connection.auth(args.username, args.password)
 	except Exception as e :
 		print(("exception msg: " + str(e)))
 
-if args.command[0] == 'hash':
+# Execute command
+if args.command == 'hash':
 	# Hash the file
 	for thisFile in fileList:
 		thisED2K = adba.aniDBfileInfo.get_ED2K(thisFile, forceHash=True)
 		print(thisED2K)
-elif args.command[0] == 'mylistadd':
+elif args.command == 'mylistadd':
 	# First try to add the file, then try to edit
 	if connection.authed():
 		for thisFile in fileList:
@@ -107,7 +124,7 @@ elif args.command[0] == 'mylistadd':
 			except:
 				episode.add_to_mylist(state=args.state, viewed=args.watched, source=args.source, storage=args.storage, other=args.other)
 				print(thisFile + " successfully added to AniDB MyList.")
-elif args.command[0] == 'mylistdel':
+elif args.command == 'mylistdel':
 	# Delete the file
 	if connection.authed():
 		for thisFile in fileList:
@@ -117,8 +134,8 @@ elif args.command[0] == 'mylistdel':
 				print(thisFile + " successfully removed from AniDB MyList.")
 			except:
 				print(thisFile + " could not be removed from AniDB MyList.")
-elif args.command[0] == 'getfields':
-	# Parse requested fields
+elif args.command == 'mylistaddwithfields':
+	# Parse requested field(s)
 	requestedFields = list(args.fields.lower().split(','))
 	# Separate fields by request type
 	maper = adba.aniDBmaper.AniDBMaper()
@@ -126,16 +143,37 @@ elif args.command[0] == 'getfields':
 	maperFileA = set(maper.getFileMapA())
 	requestF = list(maperFileF & set(requestedFields))
 	requestA = list(maperFileA & set(requestedFields))
-	# Retrieve the requested fields
+	# Add/edit the file to AniDB and print the retrieved field(s)
 	if connection.authed():
-		print('\t'.join(requestedFields))
+		for thisFile in fileList:
+			episode = adba.Episode(connection, filePath=thisFile, load=True, paramsF=requestF, paramsA=requestA)
+			try:
+				episode.edit_to_mylist(state=args.state, viewed=args.watched, source=args.source, storage=args.storage, other=args.other)
+			except:
+				episode.add_to_mylist(state=args.state, viewed=args.watched, source=args.source, storage=args.storage, other=args.other)
+			episodeDict = attrToDict(episode)
+			print("filename\t" + thisFile)
+			for field in requestedFields:
+				print(field + "\t" + str(episodeDict[field]))
+elif args.command == 'getfields':
+	# Parse requested field(s)
+	requestedFields = list(args.fields.lower().split(','))
+	# Separate fields by request type
+	maper = adba.aniDBmaper.AniDBMaper()
+	maperFileF = set(maper.getFileMapF())
+	maperFileA = set(maper.getFileMapA())
+	requestF = list(maperFileF & set(requestedFields))
+	requestA = list(maperFileA & set(requestedFields))
+	# Retrieve the requested field(s)
+	if connection.authed():
 		for thisFile in fileList:
 			episode = adba.Episode(connection, filePath=thisFile, load=True, paramsF=requestF, paramsA=requestA)
 			episodeDict = attrToDict(episode)
-			currentFields = [str(episodeDict[field]) for field in requestedFields]
-			print('\t'.join(currentFields))
-elif args.command[0] == 'listfields':
-	# Print all the possible fields that can be requested, remove blacklisted fields
+			print("filename\t" + thisFile)
+			for field in requestedFields:
+				print(field + "\t" + str(episodeDict[field]))
+elif args.command == 'listfields':
+	# Print all the possible field(s) that can be requested, remove blacklisted fields
 	maper = adba.aniDBmaper.AniDBMaper()
 	maperFileF = list(maper.getFileMapF())
 	maperFileA = list(maper.getFileMapA())
@@ -143,6 +181,8 @@ elif args.command[0] == 'listfields':
 	validFields = [field for field in allFields if field not in blacklistFields]
 	print('\t'.join(validFields))
 
-if args.command[0] in ['mylistadd', 'getfields']:
+# Logout if connection active
+if args.command in ['mylistadd', 'mylistdel', 'mylistaddwithfields', 'getfields']:
 	connection.logout(True)
+
 sys.exit(0)
